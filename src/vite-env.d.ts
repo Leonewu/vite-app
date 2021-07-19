d5f86099d2c19c896a0b43a105096b6a2f1bb381
leone <379747139@qq.com>
2021-07-19 21:42:21 +0800

    fix: vite-env.d.ts, test build successfully

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module '*.svg' {
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGAttributes<SVGElement>
+  >
+}
+declare interface Window {
+  mozRequestAnimationFrame: typeof window.requestAnimationFrame
+  mozCancelAnimationFrame: typeof window.cancelAnimationFrame
+}
