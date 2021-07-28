21a1fb559b8c822357045f3a48321021061e894f
leone <379747139@qq.com>
2021-07-28 22:52:54 +0800

    featpersist theme mode

--- src/layout/theme.ts
+++ src/layout/theme.ts
@@ -6,11 +6,16 @@ export enum THEME {
 }
 
 function useTheme(): [THEME, () => void] {
-  const [mode, setMode] = useState<THEME>(THEME.LIGHT)
+  const defaultMode = (localStorage.getItem('mode') || THEME.LIGHT) as THEME
+  const [mode, setMode] = useState<THEME>(defaultMode)
   const toggle = useCallback(() => {
     setMode(mode === THEME.LIGHT ? THEME.DARK : THEME.LIGHT)
     document.body.classList.remove(mode)
     document.body.classList.add(mode === THEME.LIGHT ? THEME.DARK : THEME.LIGHT)
+    localStorage.setItem(
+      'mode',
+      mode === THEME.LIGHT ? THEME.DARK : THEME.LIGHT
+    )
   }, [mode])
   useEffect(() => {
     document.body.classList.add(mode)
