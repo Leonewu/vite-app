438a83e82cfe2455fbd1323c8420496bde9cfe94
吴高亮 <379747139@qq.com>
2021-07-28 18:36:51 +0800

    feat: icon

--- src/layout/header/mode/index.tsx
+++ src/layout/header/mode/index.tsx
@@ -1,15 +1,9 @@
-import React, { useState } from 'react'
+import React, { useMemo } from 'react'
 import { ReactComponent as Icon } from './sun-moon.svg'
 import './index.css'
+import useTheme from '../../theme'
 
 export default () => {
-  const [state, setState] = useState<'light' | 'dark'>('light')
-  return (
-    <Icon
-      className="mode"
-      onClick={() => {
-        setState(state === 'light' ? 'dark' : 'light')
-      }}
-    />
-  )
+  const [mode, toggle] = useTheme()
+  return <Icon className={`mode ${mode}`} onClick={toggle} />
 }
